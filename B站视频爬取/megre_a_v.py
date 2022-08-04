506eb9fec53df9c569c39865a92999d1e5153908
林松鑫 <linsongxin@forchange.tech>
2022-08-04 14:05:54 +0800

    仅供个人使用

--- B站视频爬取/megre_a_v.py
+++
@@ -1,9 +0,0 @@
-import os
-path = './Bilibili/'
-for file_ptah in os.listdir(path):
-    title = path + file_ptah + '/' + os.listdir(path+file_ptah)[0].split('.')[0]
-    audio = title + '.mp3'
-    vedio = title + '.mp4'
-    output = title + '(含音频).mp4'
-
-    os.system('ffmpeg -i {} -i {} -c:v copy -c:a aac -strict experimental {}'.format(vedio, audio, output))
